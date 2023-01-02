042253aa5974d11b79cf48a6b205c9d7a3d31c2e
Andrew <kaioshin2005@yahoo.com>
2023-01-02 16:40:10 -0600

    Increased word list width

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-    import { page } from '$app/stores';
     import Words from '../../components/Words.svelte';
 </script>
 
 <h1>Word List</h1>
 <div class="list-vocab-container">
-    <!-- <div class="list-vocab"> -->
     <nav>
         <a href="../add/">Add words</a>
     </nav>
     <Words key="words" />
-    <!-- </div> -->
 </div>
 
 <style>
-
     nav {
         position: sticky;
         top: 0px;
@@ -27,9 +23,5 @@
     .list-vocab-container {
         display: flex;
         flex-direction: column;
-        /* height: 100dvh; */
-        max-width: 550px;
-        margin: 0px auto;
-        /* margin-top: 1rem; */
     }
 </style>
