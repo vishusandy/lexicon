e9b16044b2a9097e4e337fd6946f5dba06507dec
Andrew <kaioshin2005@yahoo.com>
2023-05-17 15:11:43 -0500

    added better default word list

--- src/words.ts
+++ src/words.ts
@@ -2,20 +2,18 @@ import { type Word, type WordCache, type WordList, defaultSortBy, defaultSortOrd
 import { browser } from '$app/environment';
 import { dictionaryCache } from './dictionary';
 import { cur_date } from './utils';
-// import { APIProviders, type DictionaryWord } from './dictionary';
+import { default_list } from './default_words';
 
-
-// const default_word_list: string[] = !browser ? [] : ['Malapropism', 'Eggcorn', 'Malaphor', 'Promulgate', 'Complect', 'Sluice', 'Excoriate', 'Autoantonym', 'Subsume', 'Moribund', 'Alacrity', 'Folderol', 'Rapt', 'Tacit', 'Licentious', 'Priori', 'Posteriori'];
-// const default_word_list: string[] = ['Excoriate'];
-const default_word_list: string[] = ['Admonition', 'Adroit', 'Amalgamate', 'Amenity', 'Anathema', 'Anima', 'Animus', 'Annealing', 'Antithesis', 'Aplomb', 'Appurtenant', 'Apropos', 'Arbitrage', 'Argot', 'Artifice', 'Assiduous', 'Assiduously', 'Atavistic', 'Austere', 'Auteur', 'Balaclava', 'Bastion', 'Bellicose', 'Betide', 'Bifurcate', 'Blasé', 'Blithe', 'Bolide', 'Bolster', 'Buoy', 'Buoyant', 'Cacophonous', 'Cadence', 'Canard', 'Capitulate', 'Capricious', 'Certitude', 'Cherpsed', 'Chuffed', 'Clarion', 'Commutual', 'Comparative', 'Conceit', 'Confab', 'Congruous', 'Conjoint', 'Conjunct', 'Coquettish', 'Coterie', 'Couth', 'Crudités', 'Cynosure', 'Daub', 'Demarcation', 'Derisively', 'Dexterous', 'Discomfit', 'Dodecahedron', 'Du Jour', 'Elan', 'Elucidate', 'Emollient', 'Emoluments', 'En Passant', 'Enjoin', 'Ennui', 'Ensconced', 'Epigenetics', 'Epitaph', 'Expatriate', 'Expedient', 'Foibles', 'Garrulous', 'Gradation', 'Gyre', 'Halcyon', 'Hearten', 'Hummock', 'Impenitent', 'In Situ', 'Indelible', 'Indelibly', 'Indemnification', 'Indemnity', 'Inexorable', 'Ingénue', 'Insouciant', 'Interregnum', 'Interstitial', 'Intractable', 'Intransigent', 'Jaunty', 'Joie De Vivre', 'Jovial', 'Lampoon', 'Latently', 'Livery', 'Loquacious', 'Magnanimity', 'Malaise', 'Moiety', 'Monition', 'Morose', 'Mot Juste', 'Myopic', 'Nadir', 'Nascent', 'Née', 'Nonce', 'Nonchalant', 'Nonplus', 'Obdurate', 'Oeuvre', 'Omphalos', 'Onus', 'Ostracize', 'Pablum', 'Pallor', 'Panopticon', 'Patently', 'Patina', 'Patois', 'Paucity', 'Perdurable', 'Pervasive', 'Phalanx', 'Pilloried', 'Pique', 'Pontificate', 'Portend', 'Predominant', 'Preponderant', 'Pro Tem', 'Proffer', 'Profiteroles', 'Proletariat', 'Proliferation', 'Proscribe', 'Pundit', 'Quotidian', 'Recalcitrant', 'Redound', 'Refractory', 'Rustication', 'Salient', 'Simulacrum', 'Sinew', 'Soupcon', 'Specious', 'Strata', 'Stratum', 'Substantive', 'Superbolide', 'Superlative', 'Swarthy', 'Terse', 'Titter', 'Unremitting', 'Vanguard', 'Veracity', 'Vignette', 'Vogue'];
-const defaultWords: Array<Word> = default_word_list.sort().map((word, i) => (word_cache({
-    word: word,
-    id: i,
-    def: undefined,
-    favorite: false,
-    tags: undefined,
-    dict_def: undefined,
-})));
+// const default_word_list: string[] = ['Admonition', 'Adroit', 'Amalgamate', 'Amenity', 'Anathema', 'Anima', 'Animus', 'Annealing', 'Antithesis', 'Aplomb', 'Appurtenant', 'Apropos', 'Arbitrage', 'Argot', 'Artifice', 'Assiduous', 'Assiduously', 'Atavistic', 'Austere', 'Auteur', 'Balaclava', 'Bastion', 'Bellicose', 'Betide', 'Bifurcate', 'Blasé', 'Blithe', 'Bolide', 'Bolster', 'Buoy', 'Buoyant', 'Cacophonous', 'Cadence', 'Canard', 'Capitulate', 'Capricious', 'Certitude', 'Cherpsed', 'Chuffed', 'Clarion', 'Commutual', 'Comparative', 'Conceit', 'Confab', 'Congruous', 'Conjoint', 'Conjunct', 'Coquettish', 'Coterie', 'Couth', 'Crudités', 'Cynosure', 'Daub', 'Demarcation', 'Derisively', 'Dexterous', 'Discomfit', 'Dodecahedron', 'Du Jour', 'Elan', 'Elucidate', 'Emollient', 'Emoluments', 'En Passant', 'Enjoin', 'Ennui', 'Ensconced', 'Epigenetics', 'Epitaph', 'Expatriate', 'Expedient', 'Foibles', 'Garrulous', 'Gradation', 'Gyre', 'Halcyon', 'Hearten', 'Hummock', 'Impenitent', 'In Situ', 'Indelible', 'Indelibly', 'Indemnification', 'Indemnity', 'Inexorable', 'Ingénue', 'Insouciant', 'Interregnum', 'Interstitial', 'Intractable', 'Intransigent', 'Jaunty', 'Joie De Vivre', 'Jovial', 'Lampoon', 'Latently', 'Livery', 'Loquacious', 'Magnanimity', 'Malaise', 'Moiety', 'Monition', 'Morose', 'Mot Juste', 'Myopic', 'Nadir', 'Nascent', 'Née', 'Nonce', 'Nonchalant', 'Nonplus', 'Obdurate', 'Oeuvre', 'Omphalos', 'Onus', 'Ostracize', 'Pablum', 'Pallor', 'Panopticon', 'Patently', 'Patina', 'Patois', 'Paucity', 'Perdurable', 'Pervasive', 'Phalanx', 'Pilloried', 'Pique', 'Pontificate', 'Portend', 'Predominant', 'Preponderant', 'Pro Tem', 'Proffer', 'Profiteroles', 'Proletariat', 'Proliferation', 'Proscribe', 'Pundit', 'Quotidian', 'Recalcitrant', 'Redound', 'Refractory', 'Rustication', 'Salient', 'Simulacrum', 'Sinew', 'Soupcon', 'Specious', 'Strata', 'Stratum', 'Substantive', 'Superbolide', 'Superlative', 'Swarthy', 'Terse', 'Titter', 'Unremitting', 'Vanguard', 'Veracity', 'Vignette', 'Vogue'];
+// const default_word_list: string[] = ['Admonition', 'Adroit', 'Amalgamate', 'Amenity', 'Anathema', 'Anima', 'Animus', 'Annealing', 'Antithesis', 'Aplomb', 'Appurtenant', 'Apropos', 'Arbitrage', 'Argot', 'Artifice', 'Assiduous', 'Assiduously', 'Atavistic', 'Austere', 'Auteur', 'Balaclava', 'Bastion', 'Bellicose', 'Betide', 'Bifurcate', 'Blasé', 'Blithe', 'Bolide', 'Bolster', 'Buoy', 'Buoyant'];
+// const defaultWords: Array<Word> = default_word_list.sort().map((word, i) => (word_cache({
+//     word: word,
+//     id: i,
+//     def: undefined,
+//     favorite: false,
+//     tags: undefined,
+//     dict_def: undefined,
+// })));
 
 export function new_word_cache(word: Word): WordCache {
     return {
@@ -34,7 +32,7 @@ export function word_cache(word: Word): Word {
 export function list_blank(key: string): WordList {
     let list = {
         "words": [],
-        "next_id": defaultWords.length,
+        "next_id": 1,
         "sort_by": defaultSortBy,
         "sort_order": defaultSortOrder,
         "key": key.toLowerCase()
@@ -43,29 +41,37 @@ export function list_blank(key: string): WordList {
 }
 
 function list_default(key: string): WordList {
-    let words = defaultWords;
-    let list = {
-        "words": words,
-        "next_id": defaultWords.length,
-        "sort_by": defaultSortBy,
-        "sort_order": defaultSortOrder,
-        "key": key.toLowerCase()
-    };
+    // let words = defaultWords;
+    // let list = {
+    //     "words": words,
+    //     "next_id": defaultWords.length,
+    //     "sort_by": defaultSortBy,
+    //     "sort_order": defaultSortOrder,
+    //     "key": key.toLowerCase()
+    // };
+    // list_save(list);
+    // setTimeout(() => {
+    //     addDictionaryDefintions(key);
+    // }, 200);
+    // list_save(list);
+    // return list;
+    let list = <WordList>default_list;
+    list.sort_by = defaultSortBy;
+    list.sort_order = defaultSortOrder;
+    list.key = key.toLowerCase();
+    list.next_id = list.words.length;
     list_save(list);
     return list;
 }
 
 export function list_get(key: string): WordList {
-    // if (browser) {
-    //     localStorage.removeItem(key);
-    // }
     const stored: string | null = (browser) ? localStorage.getItem(key.toLowerCase()) : null;
     let list: WordList = (stored) ? JSON.parse(stored) : list_default(key);
-    // if (browser) {
-    //     console.log('sorting: %o', list)
-    //     list_sort(list);
-    // }
     return list;
+}
+
+export function list_exists(key: string): boolean {
+    return localStorage.getItem(key.toLowerCase()) !== null;
 }
 
 export function list_save(list: WordList) {
