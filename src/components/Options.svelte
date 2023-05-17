7c8c43b9353d48fb017290278ae8671bba5a4979
Andrew <kaioshin2005@yahoo.com>
2023-05-16 22:09:57 -0500

    feat: added option to lookup definitions for undefined words

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -15,6 +15,7 @@
         if (!window.confirm('This will clear all words in your word list.  Continue?')) {
             return;
         }
+
         const blank = list_blank(key);
         list_save(blank);
     }
@@ -27,6 +28,7 @@
 
         if (!browser) return undefined;
         if (!key) return;
+
         let list: WordList = list_get(key);
         if (
             !window.confirm(
@@ -38,6 +40,7 @@
 
         let updated = 0;
         let failed: string[] = [];
+
         let promises = list.words.map(async (word) => {
             try {
                 const data = await APIProviders.default.lookup(word.word);
@@ -52,6 +55,7 @@
                 list_update(list, word, refreshWordUpdate);
             }
         });
+
         Promise.allSettled(promises).then(() => {
             if (failed.length == 0) {
                 alert = `Updated ${updated} words`;
@@ -64,13 +68,62 @@
             list_save(list);
         });
     }
+
+    function refreshUndefinedWords(key: string) {
+        function refreshWordUpdate(list_word: WordType, updated: WordType) {
+            list_word.dict_def = updated.dict_def;
+            return true;
+        }
+
+        if (!browser) return undefined;
+        if (!key) return;
+        if (!window.confirm(`Lookup definitions for words without definitions?`)) return;
+
+        let list: WordList = list_get(key);
+        let updated = 0;
+        let failed: string[] = [];
+
+        let promises = list.words
+            .filter((word) => word.dict_def === undefined)
+            .map(async (word) => {
+                try {
+                    const data = await APIProviders.default.lookup(word.word);
+                    if (data) {
+                        word.dict_def = data;
+                        updated += 1;
+                    } else {
+                        failed.push(word.word);
+                    }
+                } finally {
+                    word.cache = new_word_cache(word);
+                    list_update(list, word, refreshWordUpdate);
+                }
+            });
+
+        Promise.allSettled(promises).then(() => {
+            if (failed.length == 0) {
+                alert = `Updated ${updated} words`;
+                alert_type = 'info';
+            } else {
+                alert_type = 'error';
+                alert = `Failed to update ${failed.length} words.<br>${failed}`;
+            }
+
+            list_save(list);
+        });
+    }
 </script>
 
 <legend class="options-group-title">Options</legend>
 <div class="options-group">
     <div class="option-item">
-        <button on:click={() => refreshAllWords('words')} type="button" class="btn btn-primary"
-            >Refresh All Defintions</button
+        <button on:click={() => refreshUndefinedWords(key)} type="button" class="btn btn-primary"
+            >Find new definitions</button
+        >
+    </div>
+    <div class="option-item">
+        <button on:click={() => refreshAllWords(key)} type="button" class="btn btn-primary"
+            >Refresh all defintions</button
         >
     </div>
     <div class="option-item">
@@ -92,9 +145,5 @@
     }
     .option-item:not(:last-child) {
         margin-bottom: 1rem;
-    }
-
-    .btn-secondary {
-        border: 1px solid #999;
     }
 </style>
