7d84349963b6db39be90f40d0413daf3a7cba914
Andrew <kaioshin2005@yahoo.com>
2023-05-22 22:24:53 -0500

    Updates

---
+++ _app/immutable/chunks/3-7b0cb635.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-cf1f318d.js";export{t as component};
--- _app/immutable/chunks/3-7f81411b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-06dc3423.js";export{t as component};
